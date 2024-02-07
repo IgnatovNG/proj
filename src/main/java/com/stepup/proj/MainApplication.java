dc70cdb4d8f57a0ef1230443f6cd061139a6db69
Ignatov-NG <127387123+Ignatov-NG@users.noreply.github.com>
2024-02-07 13:58:46 +0300

    Изменен метод doSomething()

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -5,4 +5,11 @@ public class MainApplication {
         System.out.println("Hello World!");
         System.out.println("1001");
     }
+
+    public static void doSomething() {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+
+    }
 }
